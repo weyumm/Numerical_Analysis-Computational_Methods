3e8cc2dd214a71bbe7efc90e46cb0e315ac1d8d2
weyumm <160303233+weyumm@users.noreply.github.com>
2025-06-15 23:23:10 +0800

    快点补充迭代法，要不然考试零分

---
+++ 第五章-代数求根/迭代法求解方程的根[需要补充].py
@@ -0,0 +1,21 @@
+﻿#迭代法
+
+import numpy as np
+import math
+N0=int(input("请输入最大迭代次数"))
+e=float(input("请输入精度"))
+x1=float(input("请输入x1："))
+
+def g(x):  #定义转化后的函数
+    return math.sin(x)+0.5
+k=1
+while k<=N0:
+    x=g(x1)
+    if abs(x-x1)<e:
+        print("方程的解为：%s，迭代次数为：%s"%(x,k))
+        break
+    else:
+        x1=x
+    k+=1
+if k==N0+1:
+    print("在%s次迭代之后无法达到所要求的精度"%N0)
